9c382b9bb2d1637393ff0f9d311c5a61dba1d889
dhiraj4191 <dhiraj4191@users.noreply.github.com>
2016-11-29 18:17:13 +0530

    Update services.js
    changing url

--- shipwreck-mobile-app/www/js/services.js
+++ shipwreck-mobile-app/www/js/services.js
@@ -1,5 +1,5 @@
 angular.module('app.services', []).factory('Shipwreck', function($resource) {
-  return $resource('10.20.41.182:9999/api/v1/shipwrecks/:id', { id: '@id' }, {
+  return $resource('http://ec2-52-90-146-157.compute-1.amazonaws.com:9999/api/v1/shipwrecks/:id', { id: '@id' }, {
     update: {
       method: 'PUT'
     }
